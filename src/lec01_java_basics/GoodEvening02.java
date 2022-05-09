8319cfa279eff0b057c4b25a3e86cd58c17c8d98
TofaelKabir <tofaelkabir@gmail.com>
2022-05-08 22:51:07 -0400

    naming convention, java comments, print vs println, copy a line of code, renaming

---
+++ src/lec01_java_basics/GoodEvening02.java
@@ -0,0 +1,18 @@
+package lec01_java_basics;
+
+// we will learn print vs println
+// println is mostly used
+
+public class GoodEvening02 {
+	public static void main(String[] args) {
+		// Here System is a class, out is a properties, print() is a method
+		System.out.print("Good Evening! ");
+		System.out.print("How are you feeling? ");
+		System.out.print("Welcome to Enthrall IT");
+		
+		// println -- print line by line
+		// print -- print continuously, not line by line
+		// you can make it readable by putting a space at the end
+	}
+
+}
---
+++ src/lec01_java_basics/Hello.java
@@ -0,0 +1,13 @@
+package lec01_java_basics;
+
+// public - access modifier, Hello - Class name
+public class Hello {
+	// main
+	public static void main(String[] args) {
+		// syso
+		System.out.println("Hello World!");		
+	}
+}
+
+// This part is called Editor
+// Below part is called Console
