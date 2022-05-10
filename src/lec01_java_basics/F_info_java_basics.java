3963e042dfa0a27fbddbd8d79c9c03d269e28231
TofaelKabir <tofaelkabir@gmail.com>
2022-05-10 10:26:59 -0400

    snake case added

--- src/lec01_java_basics/F_info_java_basics.java
+++ src/lec01_java_basics/F_info_java_basics.java
@@ -34,6 +34,12 @@ What is an alternative to the camel case?
 -- Alternative example of Camel case, for a package --> lec01_java_basics
 -- Alternative example of Camel case, for a class --> Naming_conventions
 
+What is an alternative to the camel case called?
+Snake case (stylized as snake_case) refers to the style of writing in which each space is replaced by an 
+underscore (_) character, and the first letter of each word written in lowercase. It is a commonly used naming 
+convention in computing, for example for variable and subroutine names, and for filenames. One study has found
+that readers can recognize snake case values more quickly than camel case. 
+
 8) What is the Main method?
 -- Entry point for Java. 
 represented by - 
